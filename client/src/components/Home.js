a9e6ea9eeac94b214708e9427855d3fa497a900a
Andre <andre_dean@me.com>
2022-06-07 20:32:14 -0700

    Updated misspelling of 'Souvenir'. in the root package.json added npm install of server dependencies

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -7,7 +7,7 @@ const Home = ({stateName,updateStateName}) => {
   return (
     <div className="content-container">
       <header id="home-header">
-        <h1>Souvenier Penny Locations</h1>
+        <h1>Souvenir Penny Locations</h1>
         <div>
           <img src={pennyMachineImg} alt="" id="main-header-image" />
         </div>
@@ -19,7 +19,7 @@ const Home = ({stateName,updateStateName}) => {
         <article id="about">
           <p>It is generally accepted that the first elongated coins in the United States began at the World’s Columbian Exposition held in Chicago, Illinois in 1892-1893.</p>
   
-          <p>Here you will find crowd-sourced locations of souvenier penny machines across the United States. From the dropdown above, select the state you wish to view to see the results as a list or on a map (allow location access for best experience). If you discover a location that is not yet added you may add the location yourself for everyone else to find later.  </p>
+          <p>Here you will find crowd-sourced locations of souvenir penny machines across the United States. From the dropdown above, select the state you wish to view to see the results as a list or on a map (allow location access for best experience). If you discover a location that is not yet added you may add the location yourself for everyone else to find later.  </p>
         </article>
       </section>
     </div>
