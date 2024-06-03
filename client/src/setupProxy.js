5dc994608ecf6143e8415c5c6561ba67775b3954
Andre <andre_dean@me.com>
2024-06-03 12:57:56 -0700

    updated proxy for render deployment

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,8 +1,10 @@
-const {createProxyMiddleware} = require ("http-proxy-middleware");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
-module.exports = function(app){
-  app.use(createProxyMiddleware(["/api"], {
-    target: "https://souvenir-penny.herokuapp.com/",
-    changeOrigin: true
-  }));
+module.exports = function (app) {
+  app.use(
+    createProxyMiddleware(["/api"], {
+      target: "https://pennycollector-backend.onrender.com",
+      changeOrigin: true,
+    })
+  );
 };
