a90f7c701454c64856c913e97fa39446621ffafd
Andre <andre_dean@me.com>
2024-06-03 08:04:52 -0700

    added /api with index.js for vercel deployment

---
+++ server/api/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const db = require("./db");
+require("dotenv").config();
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(express.json({ extended: false }));
+
+app.use("/api/locations", require("../routes/api/locations"));
+
+const path = require("path");
+
+app.use(express.static(path.resolve(__dirname, "../client/build")));
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+});
+
+db.initDb((error, db) => {
+  if (error) {
+    console.error(error);
+  }
+  app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+});
