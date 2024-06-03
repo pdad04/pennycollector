7ef157e99342a783139bd6b12f651303a10daebf
Andre <andre_dean@me.com>
2024-06-03 13:40:08 -0700

    Add cors package

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(express.json({ extended: false }));
+app.use(cors());
 
 app.use("/api/locations", require("./routes/api/locations"));
 
