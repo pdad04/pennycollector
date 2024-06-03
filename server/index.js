dc1e480950c2c698201f0b0a2dc1f697b13089d4
Andre <andre_dean@me.com>
2024-06-03 12:44:20 -0700

    remove serving static file

--- server/index.js
+++ server/index.js
@@ -5,20 +5,20 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-app.use(express.json({extended: false}));
+app.use(express.json({ extended: false }));
 
 app.use("/api/locations", require("./routes/api/locations"));
 
 const path = require("path");
 
-app.use(express.static(path.resolve(__dirname, "../client/build")));
-app.get("*", function (request, response) {
-  response.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
-});
+// app.use(express.static(path.resolve(__dirname, "../client/build")));
+// app.get("*", function (request, response) {
+//   response.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+// });
 
 db.initDb((error, db) => {
-  if(error){
+  if (error) {
     console.error(error);
   }
   app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
-})
+});
